301a6ed3f5669174c655aca9eed648bb3595d253
H.Yamada <ymd_h@outlook.jp>
2024-10-13 12:35:00 +0900

    Add compat enum

--- src/compat.zig
+++ src/compat.zig
@@ -9,3 +9,5 @@ const uptoV013 = (version.major == 0) and (version.minor <= 13);
 pub const pointer = if (uptoV013) .Pointer else .pointer;
 pub const array = if (uptoV013) .Array else .array;
 pub const vector = if (uptoV013) .Vector else .vector;
+pub const int = if (uptoV013) .Int else .int;
+pub const float = if (uptoV013) .Float else .float;
