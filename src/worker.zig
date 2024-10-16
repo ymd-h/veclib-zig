80efd519583814c728f8a6231933bd7a01dd0f95
H.Yamada <ymd_h@outlook.jp>
2024-10-16 21:20:33 +0900

    Fix Worker.nullary() implementation

--- src/worker.zig
+++ src/worker.zig
@@ -146,7 +146,7 @@ pub const Worker = struct {
         }
     }
 
-    pub fn nullary(self: *Self, comptime options: math.NullaryOptions, wait_group: *WaitGroup, out: anytype) void {
+    pub fn nullary(self: *Self, comptime options: math.NullaryOptions, wait_group: *WaitGroup, out: anytype) !void {
         const T = options.type;
         var it = RecordItrator.init(T, out.len, options.simd_size, self.nThreads());
 
@@ -159,14 +159,14 @@ pub const Worker = struct {
                 const F = struct {
                     fn call(i: T, oi: []T) void {
                         math.nullary(options, oi); // iota
-                        math.unary(.{ .type = T, .simd_size = options.simd_size, .f = .add }, i, oi);
+                        math.binary(.{ .type = T, .simd_size = options.simd_size, .f = .add }, i, oi, oi);
                     }
                 };
 
                 var i: usize = 0;
                 while (it.next()) |range| {
                     const oi = out[range.start..range.end];
-                    self.spawnWg(wait_group, F.call, .{ i, oi });
+                    try self.spawnWg(wait_group, F.call, .{ i, oi });
                     i += range.end - range.start;
                 }
             },
